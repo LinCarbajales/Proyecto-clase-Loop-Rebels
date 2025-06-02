e62b4a50b602d0fa4a100c063e86b406e30b9778
Lin <lincarbajales@gmail.com>
2025-06-02 14:21:14 +0200

    Empezando a programar funcionalidad JS en index, creado archivo index-script.js

---
+++ index-script.js
@@ -0,0 +1,11 @@
+console.log("Hola, mundo");
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const boton = document.querySelector(".button-toggle");
+    const menuLinks = document.querySelector(".menu");
+
+    boton.addEventListener("click", function(){
+        menuLinks.classList.toggle("open");
+    })
+})
