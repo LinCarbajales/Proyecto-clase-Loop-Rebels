5c2fdf0691f51db58783ffa3a31fd8820f477058
Lin <lincarbajales@gmail.com>
2025-06-03 15:21:50 +0200

    Segundo menú hamburguesa en cascada

--- index-script.js
+++ index-script.js
@@ -1,17 +1,22 @@
-console.log("Hola, mundo");
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const boton = document.querySelector(".button-toggle");
     const menuLinks = document.querySelector(".burger-menu");
+
+    const conocenos = document.querySelector(".conocenos");
+    const namesMenu = document.querySelector(".names-menu");
 
     boton.addEventListener("click", function(){
         menuLinks.classList.toggle("open");
     });
 
-window.addEventListener("resize", function () {
+    conocenos.addEventListener("click", function(){
+        namesMenu.classList.toggle("open");
+    });
+
+    window.addEventListener("resize", function () {
         if (window.innerWidth > 768 && menuLinks.classList.contains("open")) {
             menuLinks.classList.remove("open");
+            namesMenu.classList.remove("open");
         }
     });
-})
+});
