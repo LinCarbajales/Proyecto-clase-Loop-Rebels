1c6f85ddece628a75a39c0b08ee7437aac1d8f73
Efren TC <efrentomascampa@gmail.com>
2025-06-04 15:03:44 +0200

    Logica del menu hamburguesa en perfiles

---
+++ Perfiles/perfil.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const boton = document.querySelector(".button-toggle");
+    const menuLinks = document.querySelector(".burger-menu");
+
+    const conocenos = document.querySelector(".conocenos");
+    const namesMenu = document.querySelector(".names-menu");
+
+    boton.addEventListener("click", function(){
+        menuLinks.classList.toggle("open");
+    });
+
+conocenos.addEventListener("mouseover", function(){
+        namesMenu.classList.add("open");
+    });
+
+    conocenos.addEventListener("click", function(){
+        namesMenu.classList.toggle("open");
+    });
+
+    window.addEventListener("resize", function () {
+        if (window.innerWidth > 768 && menuLinks.classList.contains("open")) {
+            menuLinks.classList.remove("open");
+            namesMenu.classList.remove("open");
+        }
+    });
+});
