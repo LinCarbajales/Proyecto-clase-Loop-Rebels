c1d95b7325aeea3a64cc0bce394a981f004c3897
DubinRios <dubinrioseliecerr@gmail.com>
2025-06-04 14:54:21 +0200

    cambios dubin

--- index-script.js
+++ index-script.js
@@ -9,3 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
         menuLinks.classList.toggle("open");
     })
 })
+
+document.querySelectorAll('.ficha').forEach(ficha => {
+  ficha.addEventListener('click', () => {
+    ficha.classList.toggle('flipped');
+  });
+});
+
+document.querySelectorAll('.redes a').forEach(red => {
+  red.addEventListener('click', e => {
+    e.stopPropagation();  // Detiene que el click suba al padre
+  });
+});
+
+document.querySelectorAll('.btn-flip').forEach(btn => {
+  btn.addEventListener('click', e => {
+    e.stopPropagation();
+    const ficha = btn.closest('.ficha');
+    ficha.classList.toggle('is-flipped');
+  });
+});
